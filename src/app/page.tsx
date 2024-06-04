3cce8fb6010dc9cbce7f2d1a6c426af6358ca74a
Saharsh Samir <saharshsamir10@gmail.com>
2024-06-04 16:42:45 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
 import Image from "next/image";
+import { Noto_Sans } from "next/font/google";
+import SitePreviewImage from "../../public/SitePreview.png";
+
+const notoSans = Noto_Sans({ subsets: ["latin"] });
+
+interface Props {
+    width: string
+    label: string
+    placeholder: string
+    containerClasses: string
+}
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center">
+            <div id="top-bar" className="flex justify-between w-full bg-[#141414] p-2">
+                <div className="logo-section flex items-center">
+                    <Image src="/Logo.png" alt="logo" height={120} width={120} />
+                </div>
+                <div className="linked-in-section flex items-center">
+                    <p className="text-[#999999] mr-2">Get Connected</p>
+                    <Image src="/LinkedIn.png" alt="logo" height={20} width={20} />
+                </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            </div>
+            <div id="form" className="p-3 ">
+                <p className={`text-5xl bg-gradient-to-b from-[#ffffff] to-[#2e2e2e] inline-block
+                text-transparent bg-clip-text leading-[4rem] font-bold ${notoSans.className}`}>Introducing Elecbits Flux</p>
+                <div className="w-full flex justify-center">
+                    <p className={`${notoSans.className} text-[#bfbfbf] font-bold tracking-widest`}>Source Components Like A Pro</p>
+                </div>
+                <div className="w-full flex flex-col items-center ">
+                    <form className="w-10/12 mt-8 grid grid-cols-2 gap-4 mb-10">
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                        <div className="col-span-2">
+                            <label>{"Mail ID"}</label>
+                            <div className="mt-2"></div>
+                            <input className="w-full" />
+                        </div>
+                        <div className="col-span-1">
+                            <label>{"Name"}</label>
+                            <div className="mt-2"></div>
+                            <input className="w-full" />
+                        </div>
+                        <div className="col-span-1">
+                            <label>{"Phone Number"}</label>
+                            <div className="mt-2"></div>
+                            <input className="w-full" />
+                        </div>
+                        <div className="col-span-1">
+                            <label>{"Organization Name"}</label>
+                            <div className="mt-2"></div>
+                            <input className="w-full" />
+                        </div>
+                        <div className="col-span-1">
+                            <label>{"Designation"}</label>
+                            <div className="mt-2"></div>
+                            <input className="w-full" />
+                        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                        <button className="w-full col-span-2 mt-4 bg-[#232331] hover:bg-[#3365fb] py-4 rounded-md">Join the Waitlist for Priority Access <Image className="inline-block" src={"/RightArrow.png"} alt="" width={20} height={20} /></button>
+                    </form>
+                    <p className="text-3xl font-bold">Be the First to Experience</p>
+                    <p className="text-3xl bg-gradient-to-r from-[#33E7FF] to-[#0702FC] inline-block
+                text-transparent bg-clip-text font-bold">Something Amazing</p>
+                    <Image src="/LED.png" alt={""} height={1000} width={1500} className="absolute left-1/2 translate-x-[-50%] translate-y-[30%] z-[-10]" />
+                </div>
+            </div>
+            <div className="relative w-full h-[120vh] mt-[16rem] mx-3 flex justify-center">
+                <Image src={SitePreviewImage} alt="" className="w-full h-50% absolute z-[-9] " />
+                <div className="flex p-3 z-20 w-[95%] absolute bottom-0 bg-gradient-to-r from-[#0500FF] to-[#00FFFF] justify-between bg-opacity-10">
+                    <div className="flex flex-col items-center py-4 px-5">
+                        <p className="text-xl font-bold mb-2">Real-Time Sourcing</p>
+                        <p className="text-xs text-center">Get instant quotes from 4000+ vendors & slash procurement time </p>
+                    </div>
+                    <div className="flex flex-col items-center py-4 px-5 ">
+                        <p className="text-xl font-bold mb-2">One Stop Shop</p>
+                        <p className="text-xs text-center w-2/3">Simplify Everything. Manage Components, quotes & delivery from a single platform</p>
+                    </div>
+                    <div className="flex flex-col items-center py-4 px-5">
+                        <p className="text-xl font-bold mb-2">Quality Assurance</p>
+                        <p className="text-xs text-center w-3/4">Streamline Bill of Materials with tiered solutions & data driven insights</p>
+                    </div>
+                    <div className="flex flex-col items-center py-4 px-5">
+                        <p className="text-xl font-bold mb-2">Effortless Delivery </p>
+                        <p className="text-xs text-center">Focus on your business, EB Flux handles the rest</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
