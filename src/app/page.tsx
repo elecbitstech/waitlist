7df7be10e7f52ea1de7fae5fabc1b796a4e0b260
Shivam Gavandi <shivamgavandi256@gmail.com>
2024-06-07 21:34:07 +0530

    added responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,65 +10,93 @@ import UserForm from "./form.component";
 
 const notoSans = Noto_Sans({ subsets: ["latin"] });
 
+const FooterItem = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center py-4 px-5 text-center">
+      <p className="text-lg  md:text-xl font-bold mb-2 text-white">{title}</p>
+      <p className="text-xs text-center text-white">{description}</p>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <main className="flex flex-col  items-center">
-      
-      <div className="w-full flex flex-col  items-center  " style={{ backgroundImage: `url(${formBg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div
-        id="top-bar"
-        className="flex justify-between w-full bg-[#141414] p-2"
+        className="w-full flex flex-col  items-center  "
+        style={{
+          backgroundImage: `url(${formBg.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <a className="logo-section flex items-center cursor-pointer" target="_blank" href="https://platform.elecbits.in">
-          <Image src="/Logo.png" alt="logo" height={120} width={120} />
-        </a>
-        <a className="linked-in-section flex items-center cursor-pointer" target="_blank" href="https://www.linkedin.com/company/elecbits/mycompany/">
-          <p className="text-[#999999] mr-2">Get Connected</p>
-          <Image src="/LinkedIn.png" alt="logo" height={20} width={20} />
-        </a>
-      </div>
-      <div id="form" className="p-6  ">
-        <p
-          className={`text-5xl bg-gradient-to-b from-[#ffffff] to-[#2e2e2e] inline-block text-transparent bg-clip-text leading-[4rem] font-bold ${notoSans.className}`}
+        <div
+          id="top-bar"
+          className="flex justify-between w-full bg-[#141414] p-2"
         >
-          Introducing Elecbits Flux
-        </p>
-        <div className="w-full flex justify-center">
-          <p
-            className={`${notoSans.className} text-[#bfbfbf] font-bold tracking-widest`}
+          <a
+            className="logo-section flex items-center cursor-pointer"
+            target="_blank"
+            href="https://platform.elecbits.in"
           >
-            Source Components Like A Pro
-          </p>
+            <Image src="/Logo.png" alt="logo" height={120} width={120} />
+          </a>
+          <a
+            className="linked-in-section flex items-center cursor-pointer"
+            target="_blank"
+            href="https://www.linkedin.com/company/elecbits/mycompany/"
+          >
+            <p className="text-[#999999] mr-2">Get Connected</p>
+            <Image src="/LinkedIn.png" alt="logo" height={20} width={20} />
+          </a>
         </div>
-        <div className="w-full flex flex-col items-center ">
-          <UserForm />
-          <p className="text-3xl font-bold">Be the First to Experience</p>
-          <p className="text-3xl  bg-gradient-to-r from-[#33E7FF] to-[#0702FC] inline-block text-transparent bg-clip-text font-bold">
-            Something Amazing
-          </p>
-          {/* <Image
+        <div id="form" className="p-0  md:p-6 ">
+          <div className="w-full flex flex-col items-center justify-center">
+            <p
+              className={`text-2xl md:text-5xl bg-gradient-to-b from-[#ffffff] to-[#2e2e2e] inline-block text-transparent bg-clip-text leading-[4rem] font-bold ${notoSans.className} mx-auto`}
+            >
+              Introducing Elecbits Flux
+            </p>
+            <p
+              className={`${notoSans.className} text-md md:text-lg text-[#bfbfbf] font-bold tracking-widest`}
+            >
+              Source Components Like A Pro
+            </p>
+          </div>
+          <div className="w-full flex flex-col items-center ">
+            <UserForm />
+            <p className="text-3xl font-bold">Be the First to Experience</p>
+            <p className="text-3xl  bg-gradient-to-r from-[#33E7FF] to-[#0702FC] inline-block text-transparent bg-clip-text font-bold">
+              Something Amazing
+            </p>
+            {/* <Image
             src={""}
             alt={""}
             height={1000}
             width={1500}
             className="absolute bg-black left-1/2 translate-x-[-50%] translate-y-[30%] z-[-10]"
           /> */}
-        </div>
-        <Image
+          </div>
+          <Image
             src={"./svg gradient.svg"}
             alt={""}
             height={1000}
             width={1500}
-            className="absolute bo bg-black left-1/2 translate-x-[-50%] translate-y-[10%] z-[-10]"
+            className="absolute bo bg-black left-1/2 translate-x-[-50%]  z-[-10]"
           />
+        </div>
       </div>
-      </div>
-      <div className="relative w-full h-[138vh] mt-[12rem] mx-3 flex justify-center">
+      <div className="relative w-full h-[70vh] md:h-[138vh] mt-[12rem] mx-3 flex justify-center">
         <Image
           src={SitePreviewImage}
           alt=""
-          className="w-full h-full absolute z-[-9] bg-cover"
+          className="w-full h-full absolute z-[-9] object-cover"
         />
         <Image
           src="/footerCoverIMG.png"
@@ -77,39 +105,25 @@ export default function Home() {
           width={1500}
           className="absolute bottom-0 w-full left-1/2 translate-x-[-50%] h-"
         />
-        <div className="flex p-3 mb-16 z-20 w-[95%] absolute bottom-0 bg-gradient-to-r from-[rgba(7,2,252,0.3)] to-[rgba(0,255,255,0.3)] justify-between">
-          <div className="flex flex-col items-center py-4 px-5">
-            <p className="text-xl font-bold mb-2 text-white">
-              Real-Time Sourcing
-            </p>
-            <p className="text-xs text-center text-white">
-              Get instant quotes from 4000+ vendors & slash procurement time{" "}
-            </p>
-          </div>
-          <div className="flex flex-col items-center py-4 px-5 ">
-            <p className="text-xl font-bold mb-2 text-white">One Stop Shop</p>
-            <p className="text-xs text-center w-2/3 text-white">
-              Simplify Everything. Manage Components, quotes & delivery from a
-              single platform
-            </p>
-          </div>
-          <div className="flex flex-col items-center py-4 px-5">
-            <p className="text-xl font-bold mb-2 text-white">
-              Quality Assurance
-            </p>
-            <p className="text-xs text-center w-3/4 text-white">
-              Streamline Bill of Materials with tiered solutions & data driven
-              insights
-            </p>
-          </div>
-          <div className="flex flex-col items-center py-4 px-5">
-            <p className="text-xl font-bold mb-2 text-white">
-              Effortless Delivery{" "}
-            </p>
-            <p className="text-xs text-center text-white">
-              Focus on your business, EB Flux handles the rest
-            </p>
-          </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 p-3 mb-16 z-20 w-[95%] absolute bottom-0 bg-gradient-to-r from-[rgba(7,2,252,0.5)] md:from-[rgba(7,2,252,0.3)] to-[rgba(0,255,255,0.5)] md:to-[rgba(0,255,255,0.3)] ">
+          <FooterItem
+            title="Real-Time Sourcing"
+            description="Get instant quotes from 4000+ vendors & slash procurement time"
+          />
+          <FooterItem
+            title="One Stop Shop"
+            description="Simplify Everything. Manage Components, quotes & delivery from a
+              single platform"
+          />
+          <FooterItem
+            title="Quality Assurance"
+            description="Streamline Bill of Materials with tiered solutions & data driven
+              insights"
+          />
+          <FooterItem
+            title="Effortless Delivery"
+            description="Focus on your business, EB Flux handles the rest"
+          />
         </div>
       </div>
     </main>
